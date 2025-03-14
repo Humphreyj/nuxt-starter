0f39b13d21527d88dd1f7b85a24d701ddebef36c
Josh Humphrey <joshhumphrey@Joshs-Laptop.local>
2025-03-14 14:53:36 -0500

    generate?

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,4 +10,9 @@ export default defineNuxtConfig({
     viewer: true,
     // and more...
   },
+  image: {
+    provider: 'netlify',
+    domains: ['i.postimg.cc'],
+    // other options...
+  },
 })
