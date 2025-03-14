9e702e177bbfb96a86c38d9e33d36e2533aa9076
Josh Humphrey <joshhumphrey@Joshs-Laptop.local>
2025-03-14 13:27:04 -0500

    removed prisma for now

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: [
-    '@nuxtjs/tailwindcss',
-    '@nuxt/fonts',
-    '@prisma/nuxt',
-    '@nuxt/image',
-  ],
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/fonts', '@nuxt/image'],
   tailwindcss: {
     cssPath: [`~/assets/css/tailwind.css`, { injectPosition: 'first' }],
     exposeConfig: true,
