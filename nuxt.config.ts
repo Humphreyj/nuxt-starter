b224e8575428bf8fea625742e34755153b2c53e2
Josh Humphrey <joshhumphrey@Joshs-Laptop.local>
2025-03-16 14:53:54 -0500

    changed compatibility date

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: '2025-01-01',
   devtools: { enabled: true },
   modules: ['@nuxt/image', '@nuxt/ui', '@nuxt/fonts'],
   css: ['~/assets/css/main.css'],
