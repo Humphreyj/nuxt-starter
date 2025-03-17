6aaedbc4dce0d43e1896dc0cb76777c45c3aad30
Josh Humphrey <joshhumphrey@Joshs-Laptop.local>
2025-03-17 10:03:28 -0500

    Build with different command

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/', // Ensure this is correct
+    buildAssetsDir: '/_nuxt/', // Force correct asset path
+  },
   compatibilityDate: '2025-01-01',
   devtools: { enabled: true },
   modules: ['@nuxt/image', '@nuxt/ui', '@nuxt/fonts'],
