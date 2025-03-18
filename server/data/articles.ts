7137f018453f4092eb1f551fc5d8330b4f8c44d3
Josh Humphrey <joshhumphrey1@gmail.com>
2025-03-18 12:32:28 -0500

    Fleshed out placeholder articles a little bit.

--- server/data/articles.ts
+++ server/data/articles.ts
@@ -19,7 +19,7 @@ export const articles = [
       },
     ],
     imageUrl: 'https://i.postimg.cc/Z5KSLPWs/STARFIELD-MAIN-1.jpg',
-    created_at: '2023-10-17T00:00:00.000Z',
+    created_at: '2025-03-17T00:00:00.000Z',
     author: {
       id: 0,
       name: 'Dan Good',
@@ -28,8 +28,22 @@ export const articles = [
   },
   {
     id: '7abbee39-9d8e-46dc-80bd-fb9b3a7f7fad',
-    title: 'Literally a sandbox',
-    summary: 'Play trucks with your homies.',
+    title: "Where we're going, we don't need trees.",
+    summary: 'Deforest entire regions while playing trucks with your homies.',
+    content: [
+      {
+        title: 'The Deforestation Simulator',
+        text: 'Sometimes you need a tree gone, sometimes you just suck at driving.',
+      },
+      {
+        title: 'Have an impact on the world.',
+        text: 'Leave your mark on the landscape, and the environment.',
+      },
+      {
+        title: 'I am excited to play more.',
+        text: 'I played the Roadcraft demo for about 12 hours, probably the longest I had ever spent on a demo.',
+      },
+    ],
     imageUrl: 'https://i.postimg.cc/fRwwrnrK/roadcraft-Cover.jpg',
     created_at: '2023-10-17T00:00:00.000Z',
     author: {
@@ -40,9 +54,23 @@ export const articles = [
   },
   {
     id: 'e861f901-e1b6-428f-9e0c-ac4f7b1bd315',
-    title: 'The perks of the simple life',
+    title: 'Longing for the simple life',
     summary:
       'This is not anything right now, but it could be. Fishing would be nice.',
+    content: [
+      {
+        title: 'The adventurous life can be over before it even begins.',
+        text: 'In a world as dangerous as it is beautiful, sometimes it is nice to just sit back and relax.',
+      },
+      {
+        title: 'The game does not want you to do this.',
+        text: 'I wanted to just take it easy and live the simple life, but the game had other plans. ',
+      },
+      {
+        title: 'Modders, please help.',
+        text: 'I am sure someone will make a mod that lets me fish in peace.',
+      },
+    ],
     imageUrl:
       'https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/1771300/ss_709f3329562cd2cfe6db70bac908b3e72eaef83b.1920x1080.jpg?t=1739812122',
     created_at: '2023-10-17T00:00:00.000Z',
@@ -56,7 +84,21 @@ export const articles = [
     id: '86d22bf6-bcd1-4a8d-b209-ea13ed7cd5bb',
     title: 'Bring me my brown pants!',
     summary:
-      'Sea of Thieves is a smash-hit pirate adventure game, offering the quintessential pirate experience of plundering lost treasures, intense battles, vanquishing sea monsters and more.',
+      'Set sail for a true adventure. Once you leave the dock, anything can happen.',
+    content: [
+      {
+        title: 'The sea is a dangerous place.',
+        text: 'You never know what you will find out there. It could be treasure, or it could be a giant squid.',
+      },
+      {
+        title: 'Serenity and Terror',
+        text: 'One minute you are fishing and enjoying the sunset, the next you are fighting for your life and you are on fire.',
+      },
+      {
+        title: 'Skill Issue',
+        text: 'The entire game is skill based, so if you are bad at it, you will have a bad time.',
+      },
+    ],
     imageUrl:
       'https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/1172620/ss_d431abb7120aa4a6cbc0be82e5a2cdcca62917b2.600x338.jpg?t=1740060616',
     created_at: '2023-10-17T00:00:00.000Z',
